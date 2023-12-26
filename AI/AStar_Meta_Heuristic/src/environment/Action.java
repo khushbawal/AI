d722cfd84ec81ce1c1bfd75a878706944851a76d
V1I$S5H <69389618+khushbawal@users.noreply.github.com>
2023-12-26 11:12:46 -0500

    Add files via upload

---
+++ AI/AStar_Meta_Heuristic/src/environment/Action.java
@@ -0,0 +1,14 @@
+package environment;
+
+/**
+ * A list of possible actions the robot can take.
+ * @author Khush Bawal
+ */
+public enum Action {
+	MOVE_RIGHT, 
+	MOVE_LEFT, 
+	MOVE_UP, 
+	MOVE_DOWN,
+	//CLEAN
+	DO_NOTHING;
+}
