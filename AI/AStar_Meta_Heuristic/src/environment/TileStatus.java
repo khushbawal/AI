a0d8e642aa3ebcf9589ec4024eb3fe96a38c56a2
V1I$S5H <69389618+khushbawal@users.noreply.github.com>
2023-12-26 11:14:09 -0500

    Delete AI directory

--- AI/AStar_Meta_Heuristic/src/environment/TileStatus.java
+++
@@ -1,9 +0,0 @@
-package environment;
-
-/**
- * A list of possible statuses for the environment's tiles.
- * @author Khush Bawal
- */
-public enum TileStatus {
-	CLEAN, DIRTY, IMPASSABLE, TARGET;
-}
